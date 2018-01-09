6ac63782fafb9a585d22f88ae3540261cdf8e4bb
Robert Williams <rob@r-williams.com>
2018-01-09 17:02:01 +0000

    Indentation fix

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,7 +3,7 @@ class ContactsController < ApplicationController
     respond_to do |format|
       format.csv do
         headers["X-Accel-Buffering"] = "no" # https://www.nginx.com/resources/wiki/start/topics/examples/x-accel/#x-accel-buffering
-				headers["Cache-Control"] = "no-cache" # if Cache-Control is set to anything but "no-cache" streaming will not occur
+        headers["Cache-Control"] = "no-cache" # if Cache-Control is set to anything but "no-cache" streaming will not occur
         self.response_body = Enumerator.new do |yielder|
           Contact.csv_header(yielder)
           Contact.csv_rows(yielder)
